c843d75c4e3db3df1df0b89e352d2b3ae0c0e309
taiki yamamoto <ultra0115.20@gmail.com>
2023-08-16 13:33:31 +0900

    new,confirm、index,show、edit画面作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,61 @@
 class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "新たに投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+  
+  def confirm
+    @picture = Picture.new(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+    # unless current_user == @picture.user
+    #   redirect_to pictures_path, notice: "他のユーザーの投稿は編集できません！"
+    # end
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_path, notice: "投稿を削除しました！"
+  end
+
+  private
+  
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache, :content)
+  end
+
 end
