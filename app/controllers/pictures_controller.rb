b00cd764e61c4921f24dbfb3234107327aa2863f
taiki yamamoto <ultra0115.20@gmail.com>
2023-08-17 17:13:30 +0900

    UserとPictureを紐づけ

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,6 +10,7 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
     if params[:back]
       render :new
     else
@@ -23,6 +24,7 @@ class PicturesController < ApplicationController
   
   def confirm
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
     render :new if @picture.invalid?
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :pictures
   mount_uploader :image, ImageUploader
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
